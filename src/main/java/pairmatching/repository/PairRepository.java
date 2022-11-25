1d6a7d9c517ed94e772f0daa8502b3a7e92823c6
seokjin8678 <rnrn8678@naver.com>
2022-11-26 01:55:38 +0900

    feat: 코스, 레벨, 미션을 받아 페어를 생성하는 기능 추가

--- src/main/java/pairmatching/repository/PairRepository.java
+++ src/main/java/pairmatching/repository/PairRepository.java
@@ -1,9 +1,18 @@
 package pairmatching.repository;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import pairmatching.domain.Pair;
 
 public class PairRepository {
     private final List<Pair> pairs = new ArrayList<>();
+
+    public void saveAll(Collection<Pair> pairs) {
+        this.pairs.addAll(pairs);
+    }
+
+    public void deleteAll() {
+        pairs.clear();
+    }
 }
--- src/main/java/pairmatching/service/PairMatchingService.java
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -1,13 +1,20 @@
 package pairmatching.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Collectors;
 import pairmatching.domain.Course;
 import pairmatching.domain.Crew;
+import pairmatching.domain.Mission;
+import pairmatching.domain.Pair;
 import pairmatching.repository.CrewRepository;
 import pairmatching.repository.PairRepository;
 
@@ -53,7 +60,23 @@ public class PairMatchingService {
                 .collect(Collectors.toSet());
     }
 
-    public void matchCrew() {
-
+    public List<Pair> matchCrew(Course course, Mission mission) {
+        Set<Crew> crews = crewRepository.findByCourse(course);
+        List<Pair> pairs = new ArrayList<>();
+        List<String> crewNames = crews.stream()
+                .map(Crew::getName)
+                .collect(Collectors.toList());
+        Queue<String> shuffledCrewsName = new LinkedList<>(Randoms.shuffle(crewNames));
+        while (shuffledCrewsName.size() > 0) {
+            List<Crew> crewPair = new ArrayList<>();
+            crewPair.add(new Crew(course, shuffledCrewsName.poll()));
+            crewPair.add(new Crew(course, shuffledCrewsName.poll()));
+            if (shuffledCrewsName.size() == 1) {
+                crewPair.add(new Crew(course, shuffledCrewsName.poll()));
+            }
+            pairs.add(new Pair(course, mission, crewPair));
+        }
+        pairRepository.saveAll(pairs);
+        return pairs;
     }
 }
