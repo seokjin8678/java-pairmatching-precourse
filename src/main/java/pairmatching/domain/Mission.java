64653e6e37e4fe29ab04df2a1e46d097ffce8b0f
seokjin8678 <rnrn8678@naver.com>
2022-11-25 23:28:51 +0900

    feat: Mission 열거형 추가

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,20 @@
+package pairmatching.domain;
+
+public enum Mission {
+    CAR_RACE("자동차경주", Level.LEVEL1),
+    LOTTO("로또", Level.LEVEL1),
+    NUMBER_BASEBALL("숫자야구게임", Level.LEVEL1),
+    SHOPPING_BASKET("장바구니", Level.LEVEL2),
+    PAYMENT("결제", Level.LEVEL2),
+    SUBWAY_MAP("지하철노선도", Level.LEVEL2),
+    PERFORMANCE_IMPROVE("성능개선", Level.LEVEL4),
+    DEPLOY("배포", Level.LEVEL4);
+
+    private final String name;
+    private final Level level;
+
+    Mission(String name, Level level) {
+        this.name = name;
+        this.level = level;
+    }
+}
